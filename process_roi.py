3bda9affb4f8d759d0a5e16286eb2498e69b46df
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-04-17 08:32:15 +0800

    Update process_roi.py

--- process_roi.py
+++ process_roi.py
@@ -1,23 +1,27 @@
-import os
+import gc
 from collections import deque
 import cv2
 import torch
 import numpy as np
-from PIL import Image
-from torchvision.transforms import Compose
-from depthanything.depth_anything.dpt import DepthAnything
-from dust_detect import detect_blur_fft
-from vid_lables import timestamp, draw_roi_poly
-from depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
 import torch.nn.functional as F
 
+from dust_detect import detect_blur_fft
+from stats import time_series_analysis, plot_histogram, compute_r_squared, trend_depth
+from utils import get_contours, get_centre, create_rectangle_array
+from vid_lables import timestamp, dusty_labels, draw_roi_poly, centre_labels
 
-def extract_resize_roi(frame, roi_pts, target_size=(100, 100)):
+torch.cuda.empty_cache()
+PYTORCH_CUDA_ALLOC_CONF = True
+deque_size = 50
+depth_sequence = deque(maxlen=deque_size)
+
+
+def extract_resize_roi(frame, roi_pts, target_size=(518, 518)):
     """
     Extract a region of interest (ROI) from the frame, resize it, and convert it to grayscale.
 
     Parameters:
-        frame (numpy.ndarray): The original frame.
+        frame (np.ndarray): The original frame.
         roi_pts (list): List of points defining the region of interest (ROI).
         target_size (tuple): Target size of the ROI after resizing. Default is (100, 100).
 
@@ -43,10 +47,6 @@ def create_roi_mask(frame_shape, roi_points):
     return mask
 
 
-def dusty_labels(frame, mean, dusty):
-    pass
-
-
 class FrameProcessor:
     def __init__(self, roi_comp, output_dir):
         self.roi_comp = roi_comp
@@ -55,60 +55,73 @@ class FrameProcessor:
         self.motion_start_frame = {key: 0 for key in roi_comp.rois}
         self.output_dir = output_dir
 
-    def process_frame(self, frame, prev_frame, ts):
-        mean, dusty = detect_blur_fft(frame)
-        dusty_labels(frame, mean, dusty)
-        timestamp(frame, ts)
+    def process_frame(self, raw_frame, transform, depth_anything, DEVICE, frame_height, frame_width, prev_frame, ts):
+        mean, dusty = detect_blur_fft(raw_frame)
+        # display labels on output video
+        dusty_labels(raw_frame, mean, dusty)
+        timestamp(raw_frame, ts)
+        depth_sts = []
+        depth_mean = []
+
+        r_sqr = 0
+
+        text_y = 50
 
         for roi_key in self.roi_comp.rois:
             roi = self.roi_comp.rois[roi_key]
             roi_points = roi.get_polygon_points()
-            draw_roi_poly(frame, roi_key, roi_points)
+            draw_roi_poly(raw_frame, roi_key, roi_points)
             roi_mask = create_roi_mask(prev_frame.shape, roi_points)
 
+            roi_image, mask = extract_resize_roi(raw_frame, roi_points, target_size=(100, 100))
+            cnts = get_contours(roi_mask, mask)
+            cX, cY = get_centre(cnts)
+            new_points = create_rectangle_array(cX, cY)
+
             # Extract ROI from the frame using the mask
-            roi_image, mask = extract_resize_roi(frame, roi_points, target_size=(100, 100))
+            roi_image, mask = extract_resize_roi(raw_frame, roi_points, target_size=(100, 100))
 
-            DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+            frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB) / 255.0
 
-            depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_vitl14').to(
-                DEVICE).eval()
-
-            total_params = sum(param.numel() for param in depth_anything.parameters())
-            #print('Total parameters: {:.2f}M'.format(total_params / 1e6))
-
-            transform = Compose([
-                Resize(
-                    width=1280,
-                    height=720,
-                    resize_target=False,
-                    keep_aspect_ratio=True,
-                    ensure_multiple_of=14,
-                    resize_method='lower_bound',
-                    image_interpolation_method=cv2.INTER_CUBIC,
-                ),
-                NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                PrepareForNet(),
-            ])
-
-            frame_dep = cv2.cvtColor(roi_image, cv2.COLOR_BGR2RGB) / 255.0
-
-            frame_dep = transform({'image': frame_dep})['image']
-            frame_dep = torch.from_numpy(frame_dep).unsqueeze(0).to(DEVICE)
+            frame = transform({'image': frame})['image']
+            frame = torch.from_numpy(frame).unsqueeze(0).to(DEVICE)
 
             with torch.no_grad():
-                depth = depth_anything(frame_dep)
+                depth = depth_anything(frame)
 
-            depth = F.interpolate(depth[None], (100, 100), mode='bilinear', align_corners=False)[0, 0]
+            depth = F.interpolate(depth[None],
+                                  (frame_height, frame_width),
+                                  mode='bilinear',
+                                  align_corners=False)[0, 0]
+
+            depth_sequence.append([depth, ts, roi_key])
+
+            trend_depth(depth_sequence)
+
             depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+
+            bridge_text = f"{roi_key}:    [{r_sqr * 1000}]"
+            cv2.putText(raw_frame, bridge_text, (10, text_y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255),
+                        1)
 
             depth = depth.cpu().numpy().astype(np.uint8)
             depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
 
-            depth_resized = cv2.resize(depth_color, (frame.shape[1], frame.shape[0]))
-            resized_mask = cv2.resize(mask, (frame.shape[1], frame.shape[0]))
+            if not dusty:
+                bridge_color = (0, 0, 255) if "Bridge" in bridge_text else (0, 255, 0)
+                cv2.putText(raw_frame, bridge_text, (10, text_y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, bridge_color,
+                            1)
+
+            depth_resized = cv2.resize(depth_color, (raw_frame.shape[1], raw_frame.shape[0]))
+            resized_mask = cv2.resize(mask, (raw_frame.shape[1], raw_frame.shape[0]))
 
             # Update frame with depth map for ROI
-            frame[resized_mask != 0] = depth_resized[resized_mask != 0]
+            raw_frame[resized_mask != 0] = depth_resized[resized_mask != 0]
+            centre_labels(raw_frame, roi_key, cX, cY)
 
-        return frame
+            #del transform
+            del depth
+            gc.collect()
+            text_y += 30
+
+        return raw_frame
